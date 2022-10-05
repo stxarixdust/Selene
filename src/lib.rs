4323c4beb60b0d5bdd060bd3b50644b626e5e3ec
Ari <sylveon_ari@hotmail.com>
2022-10-05 22:49:36 +0200

    Implement storing voices in Vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,95 @@
+mod voice;
+
 #[macro_use]
 extern crate vst;
 
+use std::collections::VecDeque;
 use vst::prelude::*;
 use vst::plugin::{Info, Plugin, Category};
+use rand::random;
+
+const maxVoiceCount: u8 = 16;
 
 #[derive(Default)]
-struct Selene;
+
+struct Selene {
+    sample_rate: f64,
+    voices: VecDeque<voice::Voice>
+}
+
+impl Selene {
+    fn process_midi_event(&mut self, data: [u8; 3]) {
+        match data[0] {
+            128 => self.note_off(data),
+            144 => self.note_on(data),
+            _ => (),
+        }
+    }
+
+    fn note_on(&mut self, data: [u8; 3]) {
+        self.voices.push_back(voice::Voice::new_with_velocity(data[1], data[2]));
+        if(self.voices.len() > maxVoiceCount as usize) {
+            self.voices.pop_front();
+        }
+    }
+
+    fn note_off(&mut self, data: [u8; 3]) {
+        let mut i = 0u8;
+        for v in &self.voices {
+            i += 1;
+            if v.note == data[1] {
+                self.voices.remove(i.into());
+                break;
+            }
+        }
+    }
+
+    #[allow(unused_variables)]
+    #[allow(clippy::single_match)]
+    fn process_events(&mut self, events: &Events) {
+        for event in events.events() {
+            match event {
+                Event::Midi(ev) => self.process_midi_event(ev.data),
+                // More events can be handled here.
+                _ => (),
+            }
+        }
+    }
+
+    fn set_sample_rate(&mut self, rate: f32) {
+        self.sample_rate = f64::from(rate);
+    }
+}
 
 impl Plugin for Selene {
     fn new(_host: HostCallback) -> Self {
-        Selene
+        Selene {
+            sample_rate: 44100.0,
+            voices: VecDeque::with_capacity(maxVoiceCount.into())
+        }
     }
     fn get_info(&self) -> Info {
         Info {
             name: "Selene".to_string(),
+            vendor: "Starburst Audio".to_string(),
             inputs: 0,
             outputs: 2,
             unique_id: 1337,
             category: Category::Synth,
-            ..Default::default()
+            ..Info::default()
+        }
+    }
+
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+        let samples = buffer.samples();
+        let (_, mut outputs) = buffer.split();
+        let output_count = outputs.len();
+
+        for sample_idx in 0..samples {
+            for buf_idx in 0..output_count {
+                let buff = outputs.get_mut(buf_idx);
+                buff[sample_idx] = (random::<f32>() - 0.5f32) * 2f32;
+            }
         }
     }
 }
