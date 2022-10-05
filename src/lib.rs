6681f79d28793a9b1c72de84e4d3071c9f7f1694
Ari <sylveon_ari@hotmail.com>
2022-10-05 17:29:23 +0200

    Implement new() function

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
 #[macro_use]
 extern crate vst;
 
+use vst::prelude::*;
 use vst::plugin::{Info, Plugin, Category};
 
 #[derive(Default)]
 struct Selene;
 
 impl Plugin for Selene {
+    fn new(_host: HostCallback) -> Self {
+        Selene
+    }
     fn get_info(&self) -> Info {
         Info {
             name: "Selene".to_string(),
             inputs: 0,
+            outputs: 2,
             unique_id: 1337,
-            category: Category::Synth
+            category: Category::Synth,
             ..Default::default()
         }
     }
